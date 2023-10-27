59509368b4818660d0054fb7df9e499b6e3dc643
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-28 01:06:43 +0200

    dev

--- src/components/DetailsView.vue
+++ src/components/DetailsView.vue
@@ -6,65 +6,62 @@
         @click="activeTab = 'details'"
         :class="{ active: activeTab === 'details' }"
       >
-        {{ $t("DetailsView.details") }}
+        DETAILS
       </button>
       <button
         class="tab-button"
         @click="activeTab = 'fit'"
         :class="{ active: activeTab === 'fit' }"
       >
-        {{ $t("DetailsView.fit") }}
+        FIT
       </button>
       <button
         class="tab-button"
         @click="activeTab = 'materialCare'"
         :class="{ active: activeTab === 'materialCare' }"
       >
-        {{ $t("DetailsView.m&c") }}
+        MATERIAL & CARE
       </button>
     </div>
 
     <div v-if="activeTab === 'details'">
-      <h2>{{ $t("DetailsView.details") }}</h2>
-      <p>
-        Collar: stand-up collar <br />
-        Fastener: zip <br />
-        Sleeves: long sleeves <br />
-        Pockets: inside pocket <br />
-        Details: logo <br />
-        Hem: elasticated hem <br />
-        EAN: 4099973786727 <br />
-        Article Number: 2131770.6290.S
-      </p>
+      <h2>{{ $t("DetailsView.details.elementHeader") }}</h2>
+      <ul>
+        <li>{{ $t("DetailsView.details.elementCollar") }}</li>
+        <li>{{ $t("DetailsView.details.elementFastener") }}</li>
+        <li>{{ $t("DetailsView.details.elementSleeves") }}</li>
+        <li>{{ $t("DetailsView.details.elementPockets") }}</li>
+        <li>{{ $t("DetailsView.details.elementDetails") }}</li>
+        <li>{{ $t("DetailsView.details.elementHem") }}</li>
+        <li>{{ $t("DetailsView.details.elementEAN") }}</li>
+        <li>{{ $t("DetailsView.details.elementArtNum") }}</li>
+      </ul>
     </div>
 
     <div v-if="activeTab === 'fit'">
-      <h2>{{ $t("DetailsView.fit") }}</h2>
+      <h2>{{ $t("DetailsView.fit.elementHeader") }}</h2>
       <ul>
-        <li>Fit: Regular Fit</li>
-        <li>Back length: approx. 71 cm in size L</li>
+        <li>{{ $t("DetailsView.fit.elementFit") }}</li>
+        <li>{{ $t("DetailsView.fit.elementbl") }}</li>
       </ul>
     </div>
 
     <div v-if="activeTab === 'materialCare'">
-      {{ $t("DetailsView.m&c") }}
+      {{ $t("DetailsView.m&c.elementHeader") }}
       <h2></h2>
       <ul>
-        <li>Material: woven fabric</li>
-        <li>Quality: soft</li>
-        <li>Filling: slightly padded</li>
-        <li>Level of warmth: Slightly warm</li>
+        <p>{{ $t("DetailsView.m&c.elementMaterial") }}</p>
+        <p>{{ $t("DetailsView.m&c.elementQuality") }}</p>
+        <p>{{ $t("DetailsView.m&c.elementFilling") }}</p>
+        <p>{{ $t("DetailsView.m&c.elementLevel") }}</p>
       </ul>
-      <h3>MATERIAL COMPOSITION</h3>
-      <p>Outer fabric: 100% Polyester</p>
-      <h3>CARE INSTRUCTIONS</h3>
-      <ul>
-        <li>Do not chlore</li>
-        <li>Gentle wash cycle 30°</li>
-        <li>No dry cleaning</li>
-        <li>Do not iron</li>
-        <li>Tumble with reduced thermical pressure</li>
-      </ul>
+      <h3>{{ $t("DetailsView.m&c.elementComposition") }}</h3>
+      <p>{{ $t("DetailsView.m&c.elementOuter") }}</p>
+      <h3>{{ $t("DetailsView.m&c.elementCare") }}</h3>
+      <p>{{ $t("DetailsView.m&c.elementChlore") }}</p>
+      <p>{{ $t("DetailsView.m&c.elementWash") }}</p>
+      <p>{{ $t("DetailsView.m&c.elementDry") }}</p>
+      <p>{{ $t("DetailsView.m&c.elementIron") }}</p>
     </div>
   </div>
 </template>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -24,7 +24,7 @@
       <div class="overlay-content">
         <div class="">
           <div style="height: 400px" class="bordered-div">
-            <div class="slider">
+            <div class="">
               <div v-if="navy">
                 <img
                   :src="require(`@/images/Black/${images[currentIndex]}`)"
@@ -54,8 +54,11 @@
               <button class="tab-button" @click="prevSlide">&lt;</button>
               <button class="tab-button" @click="nextSlide">&gt;</button>
             </div>
-
             <div class="description-box">
+              <button class="tab-button" @click="closeOverlay">
+                Schließen
+              </button>
+
               <h2>Description</h2>
               <div class="info-box">
                 <p>{{ name }}</p>
@@ -122,48 +125,47 @@
                     :class="{ selected: selectedSize === 'S' }"
                     @click="selectSize('S')"
                   >
-                    S
+                    {{ $t("Sizes.sizes.s") }}
                   </button>
                   <button
                     class="tab-button"
                     :class="{ selected: selectedSize === 'M' }"
                     @click="selectSize('M')"
                   >
-                    M
+                    {{ $t("Sizes.sizes.m") }}
                   </button>
                   <button
                     class="tab-button"
                     :class="{ selected: selectedSize === 'L' }"
                     @click="selectSize('L')"
                   >
-                    L
+                    {{ $t("Sizes.sizes.l") }}
                   </button>
+                  <br />
                   <button
                     class="tab-button"
                     :class="{ selected: selectedSize === 'XL' }"
                     @click="selectSize('XL')"
                   >
-                    XL
+                    {{ $t("Sizes.sizes.xl") }}
                   </button>
                   <button
                     class="tab-button"
                     :class="{ selected: selectedSize === 'XXL' }"
                     @click="selectSize('XXL')"
                   >
-                    XXL
+                    {{ $t("Sizes.sizes.xxl") }}
                   </button>
                   <button
                     class="tab-button"
+                    style="margin-bottom: -10px"
                     :class="{ selected: selectedSize === '3XL' }"
                     @click="selectSize('3XL')"
                   >
-                    3XL
+                    {{ $t("Sizes.sizes.3xl") }}
                   </button>
                 </p>
               </div>
-              <button class="tab-button" @click="closeOverlay">
-                Schließen
-              </button>
             </div>
           </div>
           <div>
@@ -172,10 +174,12 @@
                 <TabView style="width: 100%" />
               </div>
               <div class="bordered-div">
-                <div class="footer">
+                <div class="footer-overlay">
                   <div class="footer-left">{{ name }}</div>
                   <div class="footer-right">
-                    <button class="add-to-cart">Add to Cart</button>
+                    <button class="add-to-cart">
+                      {{ $t("Buttons.buttons.addToCart") }}
+                    </button>
                   </div>
                 </div>
               </div>
@@ -189,6 +193,7 @@
 
 <script setup lang="ts">
 import "@/assets/style/style.css";
+import type { ResizeObserverCallback, yourResizeHandler } from "vue";
 
 import TabView from "./DetailsView.vue";
 import { useVirtualList } from "@vueuse/core";
@@ -234,6 +239,18 @@ const selectTurquoise = () => {
   availableSizes2color.value = true;
   availableSizes3color.value = false;
 };
+
+const observerCallback: ResizeObserverCallback = (
+  entries: ResizeObserverEntry[]
+) => {
+  window.requestAnimationFrame((): void | undefined => {
+    if (!Array.isArray(entries) || !entries.length) {
+      return;
+    }
+    yourResizeHandler();
+  });
+};
+const resizeObserver = new ResizeObserver(observerCallback);
 
 const selectNavy = () => {
   turquoise.value = false;
@@ -287,7 +304,6 @@ const fetchProductData = async () => {
     name.value = firstProduct.name;
     price.value = firstProduct.price;
 
-    // Speichere die Produktinformationen im Ref-Objekt
     productData.value = data.products;
 
     console.log("data:", data);
@@ -305,5 +321,201 @@ const fetchProductData = async () => {
 onMounted(() => {
   fetchProductData();
   nextSlide();
+  resizeObserver.disconnect();
 });
 </script>
+
+<style>
+.info-box {
+  outline: #ccc;
+}
+
+.selected {
+  background-color: #b60505;
+  color: #fff;
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
+
+@media (max-width: 768px) {
+  .info-field {
+    flex-direction: column;
+  }
+}
+
+.img-responsive {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.overlay-content h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.overlay-content button {
+  margin: 10px;
+}
+
+.overlay-content input {
+  width: 100%;
+  padding: 10px;
+  margin: 10px;
+}
+
+.overlay-content {
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  overflow-y: auto;
+  max-height: 80vh;
+}
+
+.card-container {
+  width: 100%;
+  overflow-x: auto;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 300px;
+  white-space: nowrap;
+  padding: 10px;
+}
+
+.cards {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 10px;
+  flex-wrap: wrap;
+}
+
+.card {
+  flex: 0 0 auto;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  min-width: 180px;
+  max-width: 200px;
+  text-align: center;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.card-image {
+  width: 100%;
+  max-height: 100%;
+}
+
+.description {
+  font-size: 18px;
+  font-weight: bold;
+  margin: 10px 0;
+  color: #333;
+}
+
+.price {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 10px 0;
+  color: #f00;
+}
+
+@media (max-width: 768px) {
+  .overlay-content {
+    max-width: 100%;
+    padding: 10px;
+  }
+}
+
+@media (max-width: 768px) {
+  .info-field {
+    margin-top: 450px;
+    justify-content: center;
+  }
+}
+
+.info-field {
+  justify-content: center;
+  width: 100%;
+}
+.footer-overlay {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #ffffff;
+  color: #fff;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+
+.footer-left {
+  flex-grow: 1;
+  text-align: left;
+  color: #323232;
+}
+
+.add-to-cart {
+  background-color: #323232;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+@media (max-width: 768px) {
+  .footer {
+    flex-direction: column;
+    display: block;
+  }
+
+  .footer-left {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+}
+
+.bordered-div {
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.description-box {
+  flex-grow: 1;
+  padding: 20px;
+}
+
+@media (max-width: 768px) {
+  .bordered-div {
+    flex-direction: column;
+  }
+
+  .description-box {
+    margin-top: 20px;
+  }
+}
+</style>
